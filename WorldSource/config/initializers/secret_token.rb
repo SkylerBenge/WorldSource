3bab5d29326be2b7950e258c7d1062a174290b67
alex-2539 <alejandrogapan@yahoo.com>
2013-11-20 11:03:32 -0800

    Rails Setup

---
+++ WorldSource/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldSource::Application.config.secret_key_base = '37184da72bbc00667b1e839a6bf0fa1d0794d48d75b28eb3244c4234e980280071d04cdb7a59524fcbd371ae5e2c37163003f3bb6fa36cf6f05916c5dbfcb874'
